a9de407e31e4d564a275a1b52d8d344161deaafe
har <hctsicat@gmail.com>
2024-11-28 21:22:48 +0800

    macro key to clear all active layer then momentarily activate the NAV Layer

--- keyboards/ferris/keymaps/har-si_ferris_v2/keymap.c
+++ keyboards/ferris/keymaps/har-si_ferris_v2/keymap.c
@@ -57,7 +57,8 @@ enum keycodes {
   OS_ALT,
   OS_GUI,
 	OS_ALGR,
-	MC_SPTAB    // Custom Special Tab
+	MC_SPTAB,    // Custom Special Tab
+	MC_NAV       // Clear all layer and activate NAV
 };
 
 
@@ -66,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,     KC_W,     KC_E,    KC_R,    KC_T,          KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,
     KC_A,     KC_S,     KC_D,    KC_F,    KC_G,          KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN,
     KC_Z,     KC_X,     KC_C,    KC_V,    KC_B,          KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,
-                                      MO_NAV,  TD(TD_NUM_SYM), KC_SPC, OSM(MOD_LSFT)
+                                      MC_NAV,  TD(TD_NUM_SYM), KC_SPC, OSM(MOD_LSFT)
   ),
 
 
@@ -105,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,    MC_SPTAB,    __x__,    MC_ALTF4,   __x__,              KC_HOME,    KC_PGDN,   KC_PGUP,   KC_END,     KC_INS,
     KC_LCTL,   KC_LGUI,     KC_LALT,  KC_LSFT,    KC_ALGR,            KC_LEFT,    KC_DOWN,   KC_UP,     KC_RGHT,    KC_APP,
     __x__,     TO_MSE,      __x__,    CW_TOGG,    __x__,              MC_UNDO,    KC_TAB,    MC_COPY,   MC_PASTE,   KC_ENT,
-                                              TO_BSE, KC_TRNS, KC_BSPC, KC_DEL
+                                              KC_TRNS, KC_TRNS, KC_BSPC, KC_DEL
   ),
 
 
@@ -113,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,    KC_GRV,    KC_LPRN,    KC_LBRC,    KC_LCBR,             KC_PSLS,    KC_P7,    KC_P8,      KC_P9,     KC_PMNS,
     KC_LCTL,   KC_LGUI,   KC_LALT,    KC_LSFT,    KC_ALGR,             KC_PAST,    KC_P4,    KC_P5,      KC_P6,     KC_PPLS,
     __x__,     KC_MEH,    KC_RPRN,    KC_RBRC,    KC_RCBR,             KC_PDOT,    KC_P1,    KC_P2,      KC_P3,     KC_PENT,
-                                             TO_BSE, KC_TRNS,  KC_BSPC, KC_P0
+                                             KC_TRNS, KC_TRNS,  KC_BSPC, KC_P0
   ),
 
 
@@ -121,7 +122,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,    __x__,    __x__,     __x__,      __x__,                KC_WH_L,    KC_WH_D,    KC_WH_U,    KC_WH_R,     KC_INS,
     KC_LCTL,   KC_LGUI,  KC_LALT,   KC_LSFT,    KC_ALGR,              KC_MS_L,    KC_MS_D,    KC_MS_U,    KC_MS_R,     KC_BTN2,
     __x__,     KC_ACL2,  KC_ACL1,   KC_ACL0,    __x__,                KC_BTN3,    MC_CUT,     MC_COPY,    MC_PASTE,    KC_ENT,
-                                             TO_BSE, KC_ACL1, KC_BTN1, KC_DEL
+                                             KC_TRNS, KC_ACL1, KC_BTN1, KC_DEL
   ),
 
 
@@ -284,6 +285,17 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             }else {
                 return true;
             }
+
+    //Clear all active layer then momentarily activate NAV layer            
+        case MC_NAV:
+            if (record->event.pressed) {
+                layer_clear();
+                layer_on(NAV);
+            } else {
+                unregister_code(KC_TAB);
+                layer_off(NAV);
+            }
+            return false;
     }
 
     return true;
